167a4207a5122d82b6e25625a6fb4a96660f50d4
Anurag Pathak <101109228+pathakanu@users.noreply.github.com>
2023-03-03 21:03:40 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div>
+        <h1>The Earth</h1>
+        <img src="https://upload.wikimedia.org/wikipedia/commons/9/97/The_Earth_seen_from_Apollo_17.jpg" alt="Earth" />
+        <p>The Earth is a planet in the Solar System and the only known object in the Universe known to harbor life.</p>
+      </div>
+  );
+}
+
+export default App;
